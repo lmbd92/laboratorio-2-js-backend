3979de863a429e55e33ef2db7939d6f1afdc3624
seraleman <seraleman1204@gmail.com>
2022-08-14 17:48:04 -0500

    Se implementa el agregar una nueva tarea al arreglo de tareas y al html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './styles.css'
 
 import { Todo, TodoList } from './classes'
-import { crarTodoHtml } from './js/componentes'
+import { crearTodoHtml } from './js/componentes'
 
-const todoList = new TodoList()
+export const todoList = new TodoList()
 
 const tarea = new Todo('Aprender JavaScript')
 todoList.nuevoTodo(tarea)
@@ -11,4 +11,4 @@ tarea.completado = false
 
 console.log(todoList)
 
-crarTodoHtml(tarea)
+crearTodoHtml(tarea)
