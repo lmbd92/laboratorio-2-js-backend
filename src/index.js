9f1b3856e1142da7cba5767a69d0fab0aeaae64e
seraleman <seraleman1204@gmail.com>
2022-08-14 20:31:25 -0500

    Guardando y recuperando de local storage

--- src/index.js
+++ src/index.js
@@ -5,10 +5,19 @@ import { crearTodoHtml } from './js/componentes'
 
 export const todoList = new TodoList()
 
-const tarea = new Todo('Aprender JavaScript')
-todoList.nuevoTodo(tarea)
-tarea.completado = false
+// const tarea = new Todo('Aprender JavaScript')
+// todoList.nuevoTodo(tarea)
+// tarea.completado = false
+// crearTodoHtml(tarea)
 
-// console.log(todoList)
+// localStorage.setItem('mi-llave', 'ABC1234')
 
-crearTodoHtml(tarea)
+// setTimeout(() => {
+//   localStorage.removeItem('mi-llave')
+// }, 1500)
+
+// sessionStorage.setItem('mi-llave', 'ABC1234')
+
+console.log(todoList.todos)
+
+todoList.todos.forEach((todo) => crearTodoHtml(todo))
